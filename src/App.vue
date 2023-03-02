b723a0811c5a51869fd3bf27f0af2056ff7da635
llx-mbp14 <lilongxiang2000@gmail.com>
2023-03-02 22:16:46 +0800

    init: init entry

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+console.log('hello world')
+</script>
+
+<template>
+  <div>hello world</div>
+  <ul>
+    <li><a href="./asda">sadasds</a></li>
+    <li><a href="./asda">sadasds</a></li>
+    <li><a href="./asda">sadasds</a></li>
+  </ul>
+</template>
---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { createApp } from 'vue'
+
+import 'uno.css'
+
+import App from './App.vue'
+
+import './styles/main.css'
+
+
+createApp(App).mount('#app')
