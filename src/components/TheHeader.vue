92a0644cbf38829eb592c9468ddb5d57bd105324
llx-hp <lilongxiang2000@gmail.com>
2023-05-06 09:30:59 +0800

    refactor: remove repeat css

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav text-xl flex items-center mb-4 text="gray-700 dark:gray-200">
+  <nav text-xl flex items-center mb-4>
     <a href="/">Logo</a>
     <div flex-1 />
     <div flex items-center gap-2>
